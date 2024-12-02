48120c91465aaca826620a128fe148b3ce911d43
andres.rpenuela <andresruizpenuela@gmail.com>
2024-12-02 11:34:45 +0100

    [init] create project spring boot empty

---
+++ services-web/src/main/java/org/tokio/spring/servicesweb/ServicesWebApplication.java
@@ -0,0 +1,13 @@
+package org.tokio.spring.servicesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicesWebApplication.class, args);
+    }
+
+}
---
+++ services-web/src/test/java/org/tokio/spring/servicesweb/ServicesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tokio.spring.servicesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicesWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
